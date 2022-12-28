113a1cbdc42c7b5e7f4f54619f7b3673228ec9e1
Abdelmaoula <abdouder97@gmail.com>
2022-12-28 09:14:19 +0100

    main activity done

--- app/src/main/java/com/kader/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/kader/cryptocurrencyapp/presentation/MainActivity.kt
@@ -8,15 +8,37 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.kader.cryptocurrencyapp.presentation.coin_detail.CoinDetailScreen
+import com.kader.cryptocurrencyapp.presentation.coin_list.CoinListScreen
 import com.kader.cryptocurrencyapp.ui.theme.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptocurrencyAppYTTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
